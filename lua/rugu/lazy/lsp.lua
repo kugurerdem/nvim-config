9a99142e110ffbdcabb9f47a8d478d39046be1f2
kugurerdem <k.ugurerdem@gmail.com>
2024-06-13 19:04:49 +0300

    Update the LSP config code

--- lua/rugu/lazy/lsp.lua
+++ lua/rugu/lazy/lsp.lua
@@ -5,15 +5,18 @@ return {
         'williamboman/mason-lspconfig.nvim',
     },
     config = function ()
-        local servers = { 'eslint', 'lua_ls', 'rust_analyzer', 'gopls' }
+        local ensure_installed = {
+            'tsserver', 'lua_ls', 'rust_analyzer', 'gopls', 'eslint'
+        }
 
         require('mason').setup()
-        require('mason-lspconfig').setup({ ensure_installed = servers })
+        require('mason-lspconfig').setup({
+            ensure_installed = ensure_installed
+        })
 
         local on_attach = function(_, _)
             vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
@@ -21,8 +24,19 @@ return {
 
         local lspconfig = require('lspconfig')
 
-        for _, lsp in ipairs(servers) do
-            lspconfig[lsp].setup {on_attach = on_attach}
-        end
+        lspconfig.lua_ls.setup {on_attach = on_attach}
+        lspconfig.rust_analyzer.setup {on_attach = on_attach}
+        lspconfig.gopls.setup {on_attach = on_attach}
+
+        lspconfig.tsserver.setup {
+            on_attach = on_attach,
+            init_options = {
+                preferences = {
+                    disableSuggestions = true
+                }
+            }
+        }
+
+        lspconfig.eslint.setup {}
     end
 }
